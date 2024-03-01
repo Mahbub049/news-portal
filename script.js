2a51186d1477cca3ea0ded5a0c2a05220277b167
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-03-02 00:41:25 +0600

    some more features added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const categoryContainer = document.getElementById('categoryContainer');
 const showNews = document.getElementById('showNews');
+const totalItems = document.getElementById('totalItems');
+const todaysPick = document.getElementById('todaysPick');
 // const btnCategory = document.getElementById('category');
-
+let totalNews;
 let selectedCategory = '08';
 
 const dataLoad = async() => {
@@ -14,11 +16,19 @@ const dataLoad = async() => {
 const showByCategory = (data) =>{
     data.forEach(category => { 
         const categoryButton = document.createElement('button');
-        categoryButton.classList = `category px-2 py-1 rounded-md`
+        categoryButton.classList = `category catClass px-2 py-1 rounded-md`
         categoryButton.innerText = `${category.category_name}`;
         categoryContainer.appendChild(categoryButton);
         categoryButton.addEventListener('click', ()=>{
             showNewsData(category.category_id);
+            const btns = document.getElementsByClassName('catClass');
+            for(const btn of btns){
+                    btn.classList.remove('text-[#5D5FEF]')
+                    btn.classList.add('text-[#858585]')
+                }
+                categoryButton.classList.add('text-[#5D5FEF]')
+                categoryButton.classList.remove('text-[#858585]')
+
         } )
     });
 }
@@ -28,17 +38,34 @@ const showNewsData = async(id) => {
     const data = await response.json();
     const news = data.data;
     showNewsByID(news);
+    todaysPick.addEventListener('click', ()=>{
+        if(news.others_info.is_todays_pick){
+            
+        }
+    })
 }
 
 const showNewsByID = (data) =>{
     showNews.innerHTML = ``;
+    totalNews = 0;
     data.forEach(item => {
-        console.log(item);
+        const convertDate = (providedDate) =>{
+            const newDate = new Date(providedDate);
+            const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+              
+              const year = newDate.getFullYear();
+              const month = months[newDate.getMonth()];
+              const day = newDate.getDate();
+              
+              return `${month} ${day}, ${year}`;
+        }
+        totalNews++;
+        totalItems.innerText = totalNews;
         const newsDiv = document.createElement('div');
         newsDiv.classList = `flex border-2 p-5 shadow-xl mb-5 items-center`;
         newsDiv.innerHTML = `
-        <div class="w-[500px]">
-        <img src="${item.image_url}" alt="">
+        <div class="w-[244px]">
+        <img src="${item.thumbnail_url}" alt="">
         </div>
         <div class="px-10 py-5 space-y-5 flex-1">
             <h1 class="text-2xl font-bold">${item.title}</h1>
@@ -50,12 +77,12 @@ const showNewsByID = (data) =>{
                     </div>
                     <div>
                         <h4>${item.author.name = null ? 'User' : item.author.name}</h4>
-                        <p class="text-sm text-[#949494]">Jan 10, 2022</p>
+                        <p class="text-sm text-[#949494]">${convertDate(item.author.published_date)}</p>
                     </div>
                 </div>
                 <div class="flex gap-3">
                     <i class="fa-solid fa-eye text-lg"></i>
-                    <p class="text-lg font-bold">1.5M</p>
+                    <p class="text-lg font-bold">${item.total_view}</p>
                 </div>
                 <div>
                     <div class="rating">
